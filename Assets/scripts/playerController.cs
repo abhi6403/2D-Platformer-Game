351e51a1fd15101d6fbdf1b89024d34669f99460
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-09-29 06:37:33 +0530

    Update Player Controller.cs

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -5,20 +5,48 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public Animator animator;
+    //public BoxCollider2D collider;
+    private float crouchHeight = 1f;
+    private float standHeight = 2f;
 
     private void Update()
+    {
+        runAnimation();
+        crouchAnimation();
+        jumpAnimation();
+    }
+
+    private void runAnimation()
     {
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("speed", Mathf.Abs(speed));
 
         Vector3 scale = transform.localScale;
-        if(speed < 0)
+        if (speed < 0)
         {
             scale.x = -1 * Mathf.Abs(scale.x);
-        }else if (speed > 0)
+        }
+        else if (speed > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
-        transform.localScale = scale;   
+        transform.localScale = scale;
+    }
+
+    private void crouchAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetBool("Crouch", true);
+        } else
+        {
+            animator.SetBool("Crouch",false);
+        }
+    }
+    private void jumpAnimation()
+    {
+        float jump = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Jump", jump);
+
     }
 }
