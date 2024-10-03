d0fb72f828732c059973db6058914123bec65ccc
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-10-03 20:48:36 +0530

    Implemented respwan

---
+++ Assets/Scripts/GameManagerScripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    float startPosX = -9.22f;
+    float startPosY = -2.38f;
+
+    public GameObject player;
+    public Transform respawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.position;
+        }
+    }
+}
